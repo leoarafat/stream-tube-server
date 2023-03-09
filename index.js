c17149599461e8e53765922195c3410892fe2494
leoarafat10 <97153759+leoarafat10@users.noreply.github.com>
2023-03-09 18:39:08 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+//mongoDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tjc9clz.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const songsCollection = client.db("catClicker").collection("cats");
+
+async function catsRun() {
+  try {
+    
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+catsRun().catch((error) => {
+  console.log(error);
+});
+//routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
